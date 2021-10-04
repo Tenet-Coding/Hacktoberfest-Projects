d72525efc0d26a6501887766a7352e48a126083c
odorifqi <09021281621065@student.unnsri.ac.id>
2021-10-04 13:14:49 +0700

    improving error feedback

--- Commalator/script.js
+++ Commalator/script.js
@@ -2,15 +2,16 @@ let TOTAL_W = 0;
 
 function Sum() {
     let input = document.getElementById('value');
+    let total = document.getElementById('total');
 
     const sum = input.value.split(",").map((x) => Number(x)).reduce((t, v) => t + v)
 
     if (Number.isNaN(sum) || !input.value) {
         alert('Please fill the form correctly!');
         input.value = "";
-
+        total.innerHTML = "0"
         return;
     }
 
-    document.getElementById('total').innerHTML = sum;
+    total.innerHTML = sum;
 }

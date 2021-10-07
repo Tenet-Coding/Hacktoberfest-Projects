ef2161edd95ff562a8191e3c8b604bc7fcc1231f
Kaveen LK <kmklaksitha@gmail.com>
2021-10-07 23:05:15 +0530

    script and styles updated

--- Number_Guessing_Game/script.js
+++ Number_Guessing_Game/script.js
@@ -13,16 +13,12 @@ var guessedNums = []; //
 function guessTheNumber() {
   var input = document.getElementById("input").value;
   if (input < 1 || input > 20) {
-    alert("Please enter a number between 1 and 100");
+    alert("Please enter a number between 1 and 20");
   } else {
     guessedNums.push(input);
     noOfGuesses++;
 
-    if (input < answer) {
-      guessStatus.textContent = "Your guess is too low.";
-      messageGuesses.textContent = "No. of guesses: " + noOfGuesses;
-      resultGuessedNo.textContent = "Guesses numbers are:" + guessedNums;
-    } else if (input < answer + 3 && input > answer) {
+    if (input < answer + 3 && input > answer) {
       guessStatus.textContent = "You're getting close!.";
       messageGuesses.textContent = "No. of guesses: " + noOfGuesses;
       resultGuessedNo.textContent = "Guesses numbers are:" + guessedNums;
@@ -30,11 +26,15 @@ function guessTheNumber() {
       console.log("You're getting close!.");
       guessStatus.textContent = "You're getting close!.";
       messageGuesses.textContent = "No. of guesses: " + noOfGuesses;
-      resultGuessedNo.textContent = "Guesses numbers are:" + guessedNums;
+      resultGuessedNo.textContent = "Guesses numbers are: " + guessedNums;
+    } else if (input < answer) {
+      guessStatus.textContent = "Your guess is too low.";
+      messageGuesses.textContent = "No. of guesses: " + noOfGuesses;
+      resultGuessedNo.textContent = "Guesses numbers are: " + guessedNums;
     } else if (input > answer) {
       guessStatus.textContent = "Your guess is too high.";
       messageGuesses.textContent = "No. of guesses: " + noOfGuesses;
-      resultGuessedNo.textContent = "Guesses numbers are:" + guessedNums;
+      resultGuessedNo.textContent = "Guesses numbers are: " + guessedNums;
     } else if (input == answer) {
       guessStatus.textContent = "Bravo! You are correct!";
       messageGuesses.textContent = "The number was " + answer;
@@ -45,13 +45,16 @@ function guessTheNumber() {
   }
 }
 
+//to prevent strings
 window.addEventListener("keydown", function (event) {
-  if (isNaN(event.key)) {
+  var ascii = event.keyCode;
+  if (ascii >= 65 && ascii <= 90) {
     alert("Please enter a number!")
     window.location.reload();
   }
 });
 
+//to reset input when page reloads
 window.onload = function () {
   document.getElementById('input').value = '';
 }

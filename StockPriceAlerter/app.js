35d36f462958488de8c4c4905137f59ae92e2fde
VaithiSniper <vaithi.genghiskhan@gmail.com>
2021-10-16 18:10:00 +0530

    Added line and branch

--- StockPriceAlerter/app.js
+++ StockPriceAlerter/app.js
@@ -5,11 +5,11 @@ let toMail = process.env.TOMAIL;
 let subject = 'Prices';
 let text = "Prices have crossed!";
 require('dotenv').config();
-
+//imports and data setup
 fyers.setAppId(process.env.APP_ID);
 fyers.setRedirectUrl("https://localhost:3000/");
 fyers.setAccessToken(process.env.ACCESS_TOKEN)
-
+//fyers config
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -17,14 +17,14 @@ const transporter = nodemailer.createTransport({
         pass: process.env.PASSWORD
     }
     });
-
+//creating nodemailer transporter
     let mailOptions = {
         from: fromMail,
         to: toMail,
         subject: subject,
         text: text
         };
-
+//setting mail options
     async function getQuotes(){
         let quotes = new fyers.quotes()
         let result = await quotes
@@ -37,6 +37,7 @@ if (error) {
     console.log(error);
 }
 console.log(response)
-});}
-        }
-        getQuotes()
+});}}
+//function to get market quotes from fyers-api-v2		
+getQuotes();
+//function call
